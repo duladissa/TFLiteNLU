0662037a2ae6090907816372ec174ab6eb3c8611
Dulan Dissanayake <duladissa@gmail.com>
2019-09-24 19:21:39 +0800

    Updating the port.

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
 from classify import Classify
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class Employees(Resource):
+class NLU(Resource):
     classify = Classify()
     def get(self):
         return self.classify.processQuery()
@@ -17,8 +18,10 @@ class Employees(Resource):
         return {'status':'POST'}
 
 
-api.add_resource(Employees, '/query') # Route_1
+api.add_resource(NLU, '/query') # Route_1
 
+port = int(os.environ.get("PORT", 5000))
 
+print("%s%s" % ("PORT =", port))
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000)
+     app.run(host='0.0.0.0', port=port)
